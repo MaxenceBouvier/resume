879f353c48dc26b50cf68ea734fa25ac32d8a7b6
Maxence Bouvier <41475384+MaxenceBouvier@users.noreply.github.com>
2025-06-27 23:48:22 +0000

    Simplify build process by loading the image instead of building it

--- resume_build.sh
+++ resume_build.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run --rm -i -v "$PWD":/data latex pdflatex maxence_bouvier_resume.tex
+docker run --rm -i -v "$PWD":/data -w /data blang/latex pdflatex maxence_bouvier_resume.tex
